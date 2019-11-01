93dbbd626f79492d534f5b8dbf58b21277b99a9e
yamachan0625 <napoli.080625@gmail.com>
2019-11-01 14:26:14 +0900

    login-view-adjustment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,16 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_url_options = { host: 'localhost:9292' }
+  config.action_mailer.smtp_settings = {
+  :enable_starttls_auto => true,
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :domain => 'smtp.gmail.com',
+  :user_name => ENV['GMAIL_ADDRESS'] , #gmailアドレス
+  :password => ENV['GMAIL_PASSWORD'], #gmailパスワード
+  :authentication => 'login',
+}
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
