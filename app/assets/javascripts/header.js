f7884b024d0abd705b4f58eadc4233ee97f44de0
yamachan0625 <napoli.080625@gmail.com>
2019-11-23 16:28:02 +0900

    change-jquery

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,10 +1,21 @@
 $( document ).on('turbolinks:load', function() {
-  $('.my_page_btn_field').click(function(){
-    if($('.my_window').css('display') == 'none'){
-      $('.my_window').css('display','block');
-    }else{
-      $('.my_window').css('display','none');
+  // $('.my_page_btn_field').off('click')
+  // $('.my_page_btn_field').on('click', function(){
+  //   console.log('aaa')
+  //   $('.my_window').fadeIn();
+  // })
+  //   $(document).on('click', function(e) {
+  //     if (!$(e.target).closest('.my_window').length) {
+  //     $('.my_window').fadeOut();
+  //     }
+  //   })
+
+  $(document).on('click', function(e) {
+    // ２．クリックされた場所の判定
+    if($(e.target).closest('.my_page_btn_field').length){
+      $('.my_window').fadeIn();
+    }else if(!$(e.target).closest('.my_window').length ){
+      $('.my_window').fadeOut();
     }
-    
-  })
+  });
 })
