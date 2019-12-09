5d2424dcaf836a2580259282440dd4175890596e
yamachan0625 <napoli.080625@gmail.com>
2019-12-09 21:15:48 +0900

    create-DB

--- db/migrate/20191209120533_create_rooms.rb
+++ db/migrate/20191209120533_create_rooms.rb
@@ -1,6 +1,7 @@
 class CreateRooms < ActiveRecord::Migration[5.0]
   def change
     create_table :rooms do |t|
+      t.references :user
 
       t.timestamps
     end
--- db/migrate/20191209120618_create_messages.rb
+++ db/migrate/20191209120618_create_messages.rb
@@ -1,6 +1,9 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
+      t.references :user
+      t.references :room
+      t.text       :message
 
       t.timestamps
     end
