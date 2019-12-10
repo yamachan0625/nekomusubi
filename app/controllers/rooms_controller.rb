d84e7ad4eb6acbb099bd6c89aa945bcfec44b420
yamachan0625 <napoli.080625@gmail.com>
2019-12-10 17:28:35 +0900

    add-message-create-function

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -20,20 +20,24 @@ class RoomsController < ApplicationController
       @current_user_rooms << @rooms.find(i)
     end
 
+    @room_id = []
     @dm_member_id = []
     @current_user_rooms.each do |room| #ログインしているユーザーが所属しているDMをeachで回す
       room.entries.each do |room_entry| #そのDMの子要素のentriesを配列で取得しeachで回す
         unless room_entry.user_id == current_user.id #DMに所属している２名のうち自分のuser_idじゃないuserのidを
           @dm_member_id << room_entry.user_id                    #新しい配列に追加する
+          @room_id << room_entry.room_id
         else  
         end
       end
     end
 
     @dm_member = []
-    @dm_member_id.each do |member|
-      @dm_member << User.find(member)
+    @dm_member_id.each do |member| #取得したUse_idを使ってユーザーを取得し
+      @dm_member << User.find(member) #配列に追加する
     end
+
+    # binding.pry
 
     #現在ログインしているユーザーのidとそれにひもづくルームのidをwhereメソッドで探しifで条件分岐
     if Entry.where(user_id: current_user.id,room_id: @room.id).present? 
