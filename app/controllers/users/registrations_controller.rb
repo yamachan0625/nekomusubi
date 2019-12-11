27ac2bca6485139e4239a6d607ef8c3a8538f11b
yamachan0625 <napoli.080625@gmail.com>
2019-12-12 03:00:44 +0900

    Update registrations_controller.rb

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -38,5 +38,5 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end  # end
+  # end
 end
