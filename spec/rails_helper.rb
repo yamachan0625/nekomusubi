19dfdba293f95bf1d0b7151ebfa63631b79e501a
yamachan0625 <napoli.080625@gmail.com>
2020-01-06 21:53:43 +0900

    carrierwave-setting

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -42,6 +42,12 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  config.after(:all) do
+    if Rails.env.test?
+      FileUtils.rm_rf(Dir["#{Rails.root}/public/uploads_#{Rails.env}/"])
+    end
+  end
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
