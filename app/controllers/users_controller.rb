382539c8e90cf52f533f63e24d786055c2c3157b
yamachan0625 <napoli.080625@gmail.com>
2020-01-11 02:58:01 +0900

    add-message-btn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,30 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = Post.where(user_id: @user.id).order("created_at DESC").page(params[:page]).per(12)
-    @room_id = Entry.find_by(user_id: @user.id).room_id
+    if Entry.find_by(user_id: @user.id).present?
+      @room_id = Entry.find_by(user_id: @user.id).room_id
+    end
+
+
+    @currentUserEntry = Entry.where(user_id: current_user.id)
+    @userEntry = Entry.where(user_id: @user.id)
+
+    unless @user.id == current_user.id
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+      if @isRoom
+      else
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
+
   end
 
 
