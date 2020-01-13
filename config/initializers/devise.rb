e5efaedc2040e1e34644ee5892bcd192f2c2d55c
yamachan0625 <napoli.080625@gmail.com>
2020-01-13 21:14:40 +0900

    Update devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,7 +260,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-          config.omniauth :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET_KEY'],scope: 'email', info_fields: 'email', callback_url: "https://nekomikke.com/users/auth/facebook/callback"
+          config.omniauth :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET_KEY']
           config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], skip_jwt: true, scope: 'email', redirect_uri: "#{ENV['HOST']}/users/auth/google_oauth2/callback"
           config.omniauth :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET'], scope: 'email', oauth_callback: "#{ENV['HOST']}/users/auth/twitter/callback"
   # ==> Warden configuration
