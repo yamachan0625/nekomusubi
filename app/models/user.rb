ff190a26b1ad1d5f30e7a5b713a955d798ee5bc3
yamachan0625 <napoli.080625@gmail.com>
2020-01-16 15:37:53 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
           def self.from_omniauth(auth)
           where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
             user.nickname = auth.info.name
-            user.email = auth.info.email
+            user.email = User.dumy_email(auth)
             user.password = Devise.friendly_token[0,20]
           end
         end
